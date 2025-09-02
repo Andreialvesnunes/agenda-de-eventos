6f889b507c66d19350b8efcbe8690f1d9d8425e9
Andrei Alves <andreialvescvr2005@gmail.com>
2025-09-02 06:35:01 -0300

    feat(usuario): validação de entrada do nome no cadastro
    Adiciona verificação para impedir que o nome seja vazio ou ultrapasse 50 caracteres
    antes de atribuir ao objeto usuário.

--- src/controller/UsuarioController.java
+++ src/controller/UsuarioController.java
@@ -3,15 +3,30 @@ package controller;
 import model.Usuario;
 
 import java.util.Map;
+import java.util.Scanner;
 
 public class UsuarioController {
-    private Usuario usuario;
+    private Usuario usuario = new Usuario();
     protected Map<Enum, String> listaDeEventoConfirmados;
     protected Map<Enum, String> listaDeInteresse;
+    Scanner scanner = new Scanner(System.in);
 
-    public void cadastrarUsuario(){}
+    public void cadastrarUsuario(){
+        cadastrarNome();
+    }
     public void alterarInformacoesUsuario(){}
-    private void cadastrarNome(){}
+    private void cadastrarNome(){
+        String nome;
+        System.out.print("Olá, Digite seu nome: ");
+        nome = scanner.nextLine();
+
+        if (nome.isEmpty()){
+            System.out.println("Não pode estar vazio seu nome");
+        } else if (nome.length() > 50){
+            System.out.println("Seu nome ultrapassou o máximo de cáracteres(50)");
+        }
+        usuario.setNome(nome);
+    }
     private void cadastrarIdade(){}
     private void cadastrarEndereco(){}
 }
