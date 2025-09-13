0c0df50d3ed94fa4078eac34715b61a2f155f5e2
Andrei Alves <andreialvescvr2005@gmail.com>
2025-09-13 14:53:20 -0300

    feat: adicionar lógica para cadastro e alteração de informações do usuário (nome, idade e endereço)

--- src/controller/UsuarioController.java
+++ src/controller/UsuarioController.java
@@ -9,24 +9,58 @@ public class UsuarioController {
     private Usuario usuario = new Usuario();
     protected Map<Enum, String> listaDeEventoConfirmados;
     protected Map<Enum, String> listaDeInteresse;
+    private Validacao validacao = new Validacao();
     Scanner scanner = new Scanner(System.in);
 
     public void cadastrarUsuario(){
         cadastrarNome();
+        cadastrarIdade();
+        cadastrarEndereco();
     }
-    public void alterarInformacoesUsuario(){}
     private void cadastrarNome(){
         String nome;
         System.out.print("Olá, Digite seu nome: ");
         nome = scanner.nextLine();
-
-        if (nome.isEmpty()){
-            System.out.println("Não pode estar vazio seu nome");
-        } else if (nome.length() > 50){
-            System.out.println("Seu nome ultrapassou o máximo de cáracteres(50)");
-        }
+        nome = validacao.validacaoString("nome", nome, 50, scanner);
         usuario.setNome(nome);
+
     }
-    private void cadastrarIdade(){}
-    private void cadastrarEndereco(){}
+    private void cadastrarIdade(){
+        int idade;
+        System.out.println("Olá, " + usuario.getNome());
+        System.out.print("Digite sua idade: ");
+        idade = scanner.nextInt();
+
+        if (idade < 0 || idade >= 115){
+            while (idade < 0 || idade > 120){
+                System.out.println("Idade inválida");
+                System.out.print("Por favor, digitar novamente: ");
+                idade = scanner.nextInt();
+            }
+        }
+        usuario.setIdade(idade);
+    }
+    private void cadastrarEndereco(){
+        String endereco;
+        System.out.print("Digite seu endereço: ");
+        endereco = scanner.next();
+        endereco = validacao.validacaoString("endereço",endereco, 70, scanner);
+        usuario.setEndereco(endereco);
+    }
+    public void alterarInformacoesUsuario(){
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("[1] - Alterar Nome \n[2] - Alterar Idade \n[3] - Alterar Endereço");
+        System.out.print("Selecione uma opção: ");
+        int alternativa = scanner1.nextInt();
+        scanner.nextLine();
+        if (alternativa == 1){
+            cadastrarNome();
+        } else if (alternativa == 2) {
+            cadastrarIdade();
+        } else if (alternativa == 3) {
+            cadastrarEndereco();
+        } else {
+            System.out.println("Essa opção não existe");
+        }
+    }
 }
